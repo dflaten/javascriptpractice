5629a4882265ecdc7284be03a8ee55a81a75bc6a
discocorg <david.flaten@gmail.com>
2023-08-29 14:47:38 -0500

    Adding sorting objects example.

---
+++ practice/Algorithms/sortingObjects.js
@@ -0,0 +1,38 @@
+
+let employees = [
+    {name: "John Doe",
+     job: "Assistant",
+     salary: 40000
+    }, 
+    {name: "Jane Smith",
+     job: "Warehouse Worker",
+     salary: 30000
+    }, 
+    {name: "Selvester Brewster",
+     job: "Warehouse Worker",
+     salary: 30000
+    }, 
+    {name: "Da Boss",
+     job: "Warehouse Manager",
+     salary: 90000
+    }, 
+    {name: "Arty Cube",
+     job: "Sales",
+     salary: 34000
+    } 
+];
+
+function compareEmployees(a,b) {
+    if (a.salary < b.salary) { 
+        return -1;
+    }
+    if (a.salary > b.salary) {
+        return 1;
+    }
+    return 0;
+}
+
+console.log("Employees before sorting"); 
+console.log(employees);
+console.log("Employees after sorting"); 
+console.log(employees.sort(compareEmployees));
