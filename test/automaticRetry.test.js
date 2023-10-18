08ac0cd615b043568ffad6135e52a5bd5d2bc1f0
discocorg <david.flaten@gmail.com>
2023-10-18 13:19:52 -0500

    Fixed issue with imports not working correctly for automaticRetry function.

--- test/automaticRetry.test.js
+++ test/automaticRetry.test.js
@@ -1,15 +1,15 @@
-const {retry, fetchData} = require('../practice/automaticRetry');
+const {retryFunction, fetchData} = require('../practice/automaticRetry');
 
 const SUCCESSFUL_RESULT = 'Success';
 describe('Test retry function', () => {
-    const successfulFetchData = jest.fn(() => SUCCESSFUL_RESULT);
+    const successfulFetchData = jest.fn().mockResolvedValue(SUCCESSFUL_RESULT);
 
     const errorThrowingFethData = jest.fn(() => {
         throw new Error('Error has occured!');
     });
 
     it('Returns result when fetch data succeeds', async () => {
-        const result = await retry(successfulFetchData);
+        const result = await retryFunction(successfulFetchData);
         expect(result).toBe(SUCCESSFUL_RESULT);
     });
 });
