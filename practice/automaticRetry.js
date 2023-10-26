b6b5dafb4fb190cfb9a29874c32acd72915cc397
discocorg <david.flaten@gmail.com>
2023-10-26 09:28:25 -0500

    Added more test cases and functionality to retry function.

--- practice/automaticRetry.js
+++ practice/automaticRetry.js
@@ -3,26 +3,47 @@
  * be used to automatically retry requests due to networking issues.
  */
 
-async function retry(inputFunction) {
+async function retry(inputFunction, region, retryNumber) {
+    let retryAttempts = 3;
+    if (retryNumber != undefined) {
+        retryAttempts = retryNumber;
+    }     
     let attempts = 0;
     let result;
-    while (attempts < 3) {
+    while (attempts < retryAttempts) {
         try {
             attempts++;
-            result = inputFunction()
+            result = inputFunction(region);
             return result;
         } catch (error) {
             console.log("Error occurred during attempt, " + attempts + 
             " error message: " + error);
         } 
+        // If we wanted to make this interval configurable we could add 
+        // another parameter for a constant value to add or subtract from 
+        // the exponentially increasing back off. Ideally we should also 
+        // add a randomized element to this back off to avoid all
+        // retries happening arround the same time. This is sometimes
+        // called a "jitter"
+        let waitInSeconds = Math.pow(2, attempts - 1) * 100;
+        if(waitInSeconds > 1.5) {
+            waitInSeconds = 1.5;
+        }
+        await new Promise(resolve => setTimeout(resolve, waitInSeconds));
     }
     return result;
 }
 
-async function fetchData() {
+async function fetchData(location) {
     //Could throw an error or return valid result
-    resource = 'MyResult';
-    return resource;
+    if (location == 'NA') {
+        return 'NAResult';
+        
+    } else if (location == 'EU') {
+        return 'EUResult'
+    } else {
+        return 'DefaultResult'
+    }
 }
 
 async function main() {
--- test/automaticRetry.test.js
+++ test/automaticRetry.test.js
@@ -1,7 +1,14 @@
 const {retry, fetchData} = require('../practice/automaticRetry');
 
 const SUCCESSFUL_RESULT = 'Success';
+const SUCCESSFUL_FETCH_DATE_DEFAULT_RESULT = 'DefaultResult';
+const SUCCESSFUL_FETCH_DATE_NA_RESULT = 'NAResult';
 describe('Test retry function', () => {
+
+    afterEach(() => {
+        jest.clearAllMocks();
+      });
+
     const successfulFetchData = jest.fn().mockResolvedValue(SUCCESSFUL_RESULT);
 
     const errorThrowingFetchData = jest.fn(() => {
@@ -24,10 +31,16 @@ describe('Test retry function', () => {
         expect(successfulFetchData).toHaveBeenCalledTimes(1);
     });
 
-    it('Should retry a fialing function three times then throw error.', async () => {
+    it('Should retry a failing function three times then throw error.', async () => {
         const result = await retry(errorThrowingFetchData);
         expect(result).toBeUndefined();
         expect(errorThrowingFetchData).toHaveBeenCalledTimes(3);
+    });
+
+    it('Should retry a failing function 5 times then throw error.', async () => {
+        const result = await retry(errorThrowingFetchData, "NA", 5);
+        expect(result).toBeUndefined();
+        expect(errorThrowingFetchData).toHaveBeenCalledTimes(5);
     });
 
     it('Should retry a failing function and return a result after retrying twice.', async () => {
@@ -35,4 +48,17 @@ describe('Test retry function', () => {
         expect(result).toBe(SUCCESSFUL_RESULT);
         expect(errorThrowingFetchDataTwiceThenSuccess).toHaveBeenCalledTimes(3);
     });
+
+    it('Returns result when fetch data with no parameter succeeds', async () => {
+    
+        const result = await retry(fetchData);
+        expect(result).toBe(SUCCESSFUL_FETCH_DATE_DEFAULT_RESULT);
+    });
+
+    it('Returns result when fetch data with NA parameter succeeds', async () => {
+    
+        const result = await retry(fetchData, "NA");
+        expect(result).toBe(SUCCESSFUL_FETCH_DATE_NA_RESULT);
+    });
+
 });
